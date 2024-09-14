134efb35586c21371e874d10d7fec79401f6e321
gohan5858 <88976739+gohan5858@users.noreply.github.com>
2024-09-14 23:12:37 +0900

    add: memberテーブルを作成

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,5 +1,6 @@
 pub use sea_orm_migration::prelude::*;
 mod m20240914_134946_create_user;
+mod m20240914_135509_create_member;
 
 pub struct Migrator;
 
@@ -8,6 +9,7 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20240914_134946_create_user::Migration),
+            Box::new(m20240914_135509_create_member::Migration),
         ]
     }
 }
---
+++ migration/src/m20240914_135509_create_member.rs
@@ -0,0 +1,57 @@
+use sea_orm_migration::prelude::*;
+
+use crate::{m20240914_134946_create_user::User, m20240914_140343_create_group::Group};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Member::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Member::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Member::GroupId).integer().not_null())
+                    .col(ColumnDef::new(Member::UserId).integer().not_null())
+                    .col(ColumnDef::new(Member::IsAdmin).boolean().not_null())
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk_group_id")
+                            .from(Member::Table, Member::GroupId)
+                            .to(Group::Table, Group::Id),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk_user_id")
+                            .from(Member::Table, Member::UserId)
+                            .to(User::Table, User::Id),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Member::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+pub(crate) enum Member {
+    Table,
+    Id,
+    GroupId,
+    UserId,
+    IsAdmin,
+}
